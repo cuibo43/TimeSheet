f08715591363d7076538ed0edd29aabf91365051
James <cuibo43@gmail.com>
2020-01-30 10:30:01 -0500

    basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
+import { LoginComponent } from './login/login.component';
+import { TimeSheetComponent } from './time-sheet/time-sheet.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SummaryComponent } from './summary/summary.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'summary', component: SummaryComponent},
+  { path: 'timeSheet', component: TimeSheetComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: 'login', component: LoginComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './summary/summary.component';
+import { TimeSheetComponent } from './time-sheet/time-sheet.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { WebService } from './web.service';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    TimeSheetComponent,
+    ProfileComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [WebService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
