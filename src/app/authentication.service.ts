24fbfdc85bc0efd55b1aa5a36b872be16cadcf30
James <cuibo43@gmail.com>
2020-02-01 17:34:06 -0500

    auth

---
+++ src/app/authentication.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor() { }
+  setToken(UserName,token){
+
+    sessionStorage.setItem('username', UserName);
+    sessionStorage.setItem('token', token);
+  }
+
+  isUserLoggedIn() {
+    const user = sessionStorage.getItem('username');
+    return !(user === null);
+  }
+
+  logOut() {
+    sessionStorage.removeItem('username');
+  }
+}
